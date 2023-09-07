a6a906a8bd951987a32db1f82a889c91a2c7232c
AmmaraRizvi32 <ammararizvi314@gmail.com>
2023-09-06 20:50:06 -0700

    chnges

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -1,10 +1,55 @@
-import React from 'react'
-// import { AppBar } from '@mui/material'
+import React from "react";
+import { useState } from "react";
+
+import {
+  Button,
+  FormGroup,
+  FormControl,
+  InputLabel,
+  Input,
+  Container
+} from "@mui/material";
+import { useEffect } from "react";
+
 
 const Form = () => {
+  const [name, setname] = useState('Zubair')
+  let funct=()=>{
+    setname('Ammara')
+    console.log(name);
+  }
+  // useEffect(()=>{
+  //   funct()
+  // })
+  // funct()
   return (
-    <div>Form</div>
-  )
-}
+    <>
+    <Container>
+      <h1>{name}</h1>
+      <FormGroup className='form'>
+        <FormControl className="formFirld">
+          <InputLabel>Full Name</InputLabel>
+          <Input onChange={(e)=>setname(e.target.value)}/>
+        </FormControl>
+        <FormControl className="formFirld">
+          <InputLabel>Email</InputLabel>
+          <Input />
+        </FormControl>
+        <FormControl className="formFirld">
+          <InputLabel>Phone number</InputLabel>
+          <Input />
+        </FormControl>
+        <FormControl className="formFirld">
+          <InputLabel>Address</InputLabel>
+          <Input />
+        </FormControl>
+        <Button variant="contained" color="warning">
+          Submit
+        </Button>
+      </FormGroup>
+      </Container>
+    </>
+  );
+};
 
-export default Form
+export default Form;
