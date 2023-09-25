9f524d2d1fe9ab76f2896bc12e694350f38d0aa2
AmmaraRizvi32 <ammararizvi314@gmail.com>
2023-09-25 10:21:51 -0700

    commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import "./App.css";
-// import Home from "./Components/Home";
 import Navbar from "./Components/Navbar";
-import Form from "./Components/Form";
 import Footer from './Components/Footer'
 function App() {
   return (
     <div>
-      <Navbar title="Nutrals" />
-      {/* <Home /> */}
-      {/* <Form /> */}
+      <Navbar />
+
       <Footer />
     </div>
   );
